90556f1bcb08ea774b19d1ac79b016fc6c2f88c7
Mikhail Golubev-Ciuchea <mgolubev@qti.qualcomm.com>
2024-09-09 17:25:47 +0200

    Revert 523796808ae9de6f8fc778a2f5599a8314989189 "vrend: cache GL_DRAW_INDIRECT_BUFFER binding"
    Revert the commit to avoid skipping of indirect buffer binding when a
    buffer is unbound in vrend_create_buffer().
    
    Issue: COQOS-20178
    Change-Id: Ib34f170308e8c57848d45b3933a96f083557ed89

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -784,8 +784,6 @@ struct vrend_sub_context {
 
    int last_shader_idx;
 
-   GLint draw_indirect_buffer;
-
    GLint draw_indirect_params_buffer;
 
    struct pipe_rasterizer_state hw_rs_state;
@@ -6122,11 +6120,10 @@ int vrend_draw_vbo(struct vrend_context *ctx,
    }
 
    if (has_feature(feat_indirect_draw)) {
-      GLint buf = indirect_res ? indirect_res->gl_id : 0;
-      if (sub_ctx->draw_indirect_buffer != buf) {
-         glBindBuffer(GL_DRAW_INDIRECT_BUFFER, buf);
-         sub_ctx->draw_indirect_buffer = buf;
-      }
+      if (indirect_res)
+         glBindBuffer(GL_DRAW_INDIRECT_BUFFER, indirect_res->gl_id);
+      else
+         glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0);
 
       if (has_feature(feat_indirect_params)) {
          GLint buf = indirect_params_res ? indirect_params_res->gl_id : 0;
