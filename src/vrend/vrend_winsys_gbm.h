9e384ed726a21ef4e92aefd2ce336fca1021ee22
Maciej Szymanski <mszymans@qti.qualcomm.com>
2024-10-04 14:57:24 +0200

    vrend: Define missing gbm formats.
    Issue: E0997-2325
    Change-Id: I5dd4eda6a6314458a54e2a6fc57fb4a8ef06b681

--- src/vrend/vrend_winsys_gbm.h
+++ src/vrend/vrend_winsys_gbm.h
@@ -35,6 +35,16 @@
 #define VIRGL_GBM_MAX_PLANES 4
 #endif
 
+/* GBM_FORMAT_R8 */
+#ifndef GBM_FORMAT_R8
+#define GBM_FORMAT_R8		__gbm_fourcc_code('R', '8', ' ', ' ') /* [7:0] R */
+#endif
+
+/* GBM_FORMAT_XBGR16161616F */
+#ifndef GBM_FORMAT_XBGR16161616F
+#define GBM_FORMAT_XBGR16161616F __gbm_fourcc_code('X', 'B', '4', 'H') /* [63:0] x:B:G:R 16:16:16:16 little endian */
+#endif
+
 /* GBM_FORMAT_ABGR16161616F was added since mesa 20.0 */
 #ifndef GBM_FORMAT_ABGR16161616F
 #define GBM_FORMAT_ABGR16161616F __gbm_fourcc_code('A', 'B', '4', 'H') /* [63:0] A:B:G:R 16:16:16:16 little endian */
